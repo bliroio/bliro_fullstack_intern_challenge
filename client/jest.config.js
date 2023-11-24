d98b1270b6af637364c159850646fc115da9973d
Vinit <vinit224488@gmail.com>
2023-11-24 15:37:35 +0530

    feat: setup jest and test config

---
+++ client/jest.config.js
@@ -0,0 +1,20 @@
+const nextJest = require('next/jest');
+const { defaults } = require('jest-config');
+
+const createJestConfig = nextJest({
+    dir: './',
+});
+
+const customJestConfig = {
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+    testEnvironment: 'jest-environment-jsdom',
+    snapshotSerializers: [],
+    transform: {
+        ...defaults.transform,
+        '^.+\\.tsx?$': 'ts-jest',
+    },
+    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+};
+
+module.exports = createJestConfig(customJestConfig);
