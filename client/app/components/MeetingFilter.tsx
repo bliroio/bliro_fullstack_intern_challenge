8005a2d544122a25c6bd3cbef11708b8b2fd01d1
Vinit <vinit224488@gmail.com>
2023-11-24 14:52:15 +0530

    feat: Implement filtering and sorting in meetings list

---
+++ client/app/components/MeetingFilter.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import TextField from '@mui/material/TextField';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import Box from '@mui/material/Box';
+
+interface MeetingFilterProps {
+  filter: string;
+  sort: string;
+  onFilterChange: (filter: string) => void;
+  onSortChange: (sort: string) => void;
+}
+
+const MeetingFilter: React.FC<MeetingFilterProps> = ({ filter, sort, onFilterChange, onSortChange }) => {
+  return (
+    <Box sx={{ display: 'flex', justifyContent: 'space-between', marginBottom: '1rem', p: 1 }}>
+      <TextField 
+        label="Filter by title" 
+        value={filter} 
+        onChange={event => onFilterChange(event.target.value.toLowerCase())} 
+        helperText="Enter a title to filter meetings"
+      />
+      <FormControl>
+        <InputLabel id="sort-label">Sort by</InputLabel>
+        <Select 
+          labelId="sort-label" 
+          value={sort} 
+          onChange={event => onSortChange(event.target.value)}
+        >
+          <MenuItem value="title">Title</MenuItem>
+          <MenuItem value="date">Date</MenuItem>
+          <MenuItem value="participants">Number of Participants</MenuItem>
+        </Select>
+      </FormControl>
+    </Box>
+  );
+};
+
+export default MeetingFilter;
--- client/app/components/meetingList.tsx
+++ client/app/components/meetingList.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import { getMeetings } from '../services/meetingService';
 import { Meeting } from '../models/Meeting';
 import List from '@mui/material/List';
@@ -8,6 +8,7 @@ import Paper from '@mui/material/Paper';
 import Divider from '@mui/material/Divider';
 import MeetingDetails from './meetingDetail';
 import { MeetingContext } from '../contexts/MeetingContext';
+import MeetingFilter from './MeetingFilter';
 
 const formatDate = (dateString: string) => {
   const date = new Date(dateString);
@@ -19,8 +20,10 @@ const formatDate = (dateString: string) => {
 };
 
 const MeetingList: React.FC = () => {
-  const { meetings, setMeetings } = React.useContext(MeetingContext);
+  const { meetings, setMeetings } = useContext(MeetingContext);
   const [selectedMeeting, setSelectedMeeting] = useState<Meeting | null>(null);
+  const [filter, setFilter] = useState('');
+  const [sort, setSort] = useState('title');
 
   useEffect(() => {
     getMeetings().then(setMeetings);
@@ -42,23 +45,37 @@ const MeetingList: React.FC = () => {
 
   return (
     <Paper elevation={3}>
+      <MeetingFilter filter={filter} sort={sort} onFilterChange={setFilter} onSortChange={setSort} />
       <List>
-        {meetings.map((meeting, index) => (
-          <React.Fragment key={meeting._id}>
-            <Button onClick={() => setSelectedMeeting(meeting)}>
-              <ListItemText
-                primary={meeting.title}
-                secondary={
-                  <>
-                    <div>Start: {formatDate(meeting.startTime)}</div>
-                    <div>End: {formatDate(meeting.endTime)}</div>
-                  </>
-                }
-              />
-            </Button>
-            {index < meetings.length - 1 && <Divider variant="inset" component="li" />}
-          </React.Fragment>
-        ))}
+        {meetings
+          .filter(meeting => meeting.title.toLowerCase().includes(filter))
+          .sort((a, b) => {
+            if (sort === 'title') {
+              return a.title.localeCompare(b.title);
+            } else if (sort === 'date') {
+              return new Date(a.startTime).getTime() - new Date(b.startTime).getTime();
+            } else if (sort === 'participants') {
+              return a.participants.length - b.participants.length;
+            } else {
+              return 0;
+            }
+          })
+          .map((meeting, index) => (
+            <React.Fragment key={meeting._id}>
+              <Button onClick={() => setSelectedMeeting(meeting)}>
+                <ListItemText
+                  primary={meeting.title}
+                  secondary={
+                    <>
+                      <div>Start: {formatDate(meeting.startTime)}</div>
+                      <div>End: {formatDate(meeting.endTime)}</div>
+                    </>
+                  }
+                />
+              </Button>
+              {index < meetings.length - 1 && <Divider variant="inset" component="li" />}
+            </React.Fragment>
+          ))}
       </List>
     </Paper>
   );
