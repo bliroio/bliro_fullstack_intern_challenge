f4a23b9757b5d1ebd3d51d3450bb182a656157e8
Vinit <vinit224488@gmail.com>
2023-11-24 15:02:24 +0530

    Refactor: meetingList for for better performance and readability

--- client/app/components/meetingList.tsx
+++ client/app/components/meetingList.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState, useContext } from 'react';
 import { getMeetings } from '../services/meetingService';
-import { Meeting } from '../models/Meeting';
 import List from '@mui/material/List';
 import Button from '@mui/material/Button';
 import ListItemText from '@mui/material/ListItemText';
@@ -9,6 +8,7 @@ import Divider from '@mui/material/Divider';
 import MeetingDetails from './meetingDetail';
 import { MeetingContext } from '../contexts/MeetingContext';
 import MeetingFilter from './MeetingFilter';
+import { Meeting } from '../models/Meeting';
 
 const formatDate = (dateString: string) => {
   const date = new Date(dateString);
@@ -19,7 +19,7 @@ const formatDate = (dateString: string) => {
   return formatter.format(date);
 };
 
-const MeetingList: React.FC = () => {
+const MeetingList = () => {
   const { meetings, setMeetings } = useContext(MeetingContext);
   const [selectedMeeting, setSelectedMeeting] = useState<Meeting | null>(null);
   const [filter, setFilter] = useState('');
@@ -30,12 +30,14 @@ const MeetingList: React.FC = () => {
   }, []);
 
   const handleEdit = (updatedMeeting: Meeting) => {
-    setMeetings(meetings.map(meeting => meeting._id === updatedMeeting._id ? updatedMeeting : meeting));
+    const updatedMeetings = meetings.map(meeting => meeting._id === updatedMeeting._id ? updatedMeeting : meeting);
+    setMeetings(updatedMeetings);
     setSelectedMeeting(null);
   };
 
   const handleDelete = (id: string) => {
-    setMeetings(meetings.filter(meeting => meeting._id !== id));
+    const updatedMeetings = meetings.filter(meeting => meeting._id !== id);
+    setMeetings(updatedMeetings);
     setSelectedMeeting(null);
   };
 
@@ -43,39 +45,39 @@ const MeetingList: React.FC = () => {
     return <MeetingDetails meeting={selectedMeeting} onEdit={handleEdit} onDelete={handleDelete} />;
   }
 
+  const filteredMeetings = meetings.filter(meeting => meeting.title.toLowerCase().includes(filter));
+  const sortedMeetings = filteredMeetings.sort((a, b) => {
+    if (sort === 'title') {
+      return a.title.localeCompare(b.title);
+    } else if (sort === 'date') {
+      return new Date(a.startTime).getTime() - new Date(b.startTime).getTime();
+    } else if (sort === 'participants') {
+      return a.participants.length - b.participants.length;
+    } else {
+      return 0;
+    }
+  });
+
   return (
     <Paper elevation={3}>
       <MeetingFilter filter={filter} sort={sort} onFilterChange={setFilter} onSortChange={setSort} />
       <List>
-        {meetings
-          .filter(meeting => meeting.title.toLowerCase().includes(filter))
-          .sort((a, b) => {
-            if (sort === 'title') {
-              return a.title.localeCompare(b.title);
-            } else if (sort === 'date') {
-              return new Date(a.startTime).getTime() - new Date(b.startTime).getTime();
-            } else if (sort === 'participants') {
-              return a.participants.length - b.participants.length;
-            } else {
-              return 0;
-            }
-          })
-          .map((meeting, index) => (
-            <React.Fragment key={meeting._id}>
-              <Button onClick={() => setSelectedMeeting(meeting)}>
-                <ListItemText
-                  primary={meeting.title}
-                  secondary={
-                    <>
-                      <div>Start: {formatDate(meeting.startTime)}</div>
-                      <div>End: {formatDate(meeting.endTime)}</div>
-                    </>
-                  }
-                />
-              </Button>
-              {index < meetings.length - 1 && <Divider variant="inset" component="li" />}
-            </React.Fragment>
-          ))}
+        {sortedMeetings.map((meeting, index) => (
+          <React.Fragment key={meeting._id}>
+            <Button onClick={() => setSelectedMeeting(meeting)}>
+              <ListItemText
+                primary={meeting.title}
+                secondary={
+                  <>
+                    <div>Start: {formatDate(meeting.startTime)}</div>
+                    <div>End: {formatDate(meeting.endTime)}</div>
+                  </>
+                }
+              />
+            </Button>
+            {index < sortedMeetings.length - 1 && <Divider variant="inset" component="li" />}
+          </React.Fragment>
+        ))}
       </List>
     </Paper>
   );
